11d224ce03722786eb419d62cca0dac3d78c9adc
Sebastian Hoß <seb@xn--ho-hia.de>
2023-11-10 15:26:32 +0100

    simplify code generation
    Signed-off-by: Sebastian Hoß <seb@xn--ho-hia.de>

--- code-generator/adjust-cargo-toml.sh
+++ code-generator/adjust-cargo-toml.sh
@@ -7,15 +7,7 @@
 sed -i '/\[features\]/,$d' ./kube-custom-resources-rs/Cargo.toml
 echo '[features]' >>./kube-custom-resources-rs/Cargo.toml
 
-for mld in $(find ./kube-custom-resources-rs/src -type d | LC_ALL=C sort --general-numeric-sort); do
-  module=$(basename "${mld}")
-
-  if [ -f "${mld}/mod.rs" ]; then
-    echo "${module} = []" >>./kube-custom-resources-rs/Cargo.toml
-  fi
-done
-
-  find ./kube-custom-resources-rs/src -type f -name 'mod.rs' -print0 | LC_ALL=C sort --zero-terminated | while IFS= read -r -d '' file; do
-      module=$(dirname "${file}")
-      echo "${module} = []"
-  done
+find ./kube-custom-resources-rs/src -type f -name 'mod.rs' -print0 | LC_ALL=C sort --zero-terminated | while IFS= read -r -d '' file; do
+    module=$(basename "$(dirname "${file}")")
+    echo "${module} = []"
+done >> ./kube-custom-resources-rs/Cargo.toml
