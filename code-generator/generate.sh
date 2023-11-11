795712c59a291e3e15a40e5136b3e62dc943d351
Sebastian Hoß <seb@xn--ho-hia.de>
2023-11-11 08:27:19 +0100

    rename scripts to make tab completion easier
    Signed-off-by: Sebastian Hoß <seb@xn--ho-hia.de>

--- code-generator/generate.sh
+++ code-generator/generate.sh
@@ -8,7 +8,7 @@ FILTER="${1:-}"
 cargo run --package code-generator --bin crd_v1_fetcher "${FILTER}"
 cargo run --package code-generator --bin dep5_generator
 find ./crd-catalog -name 'fixup.sh' -type f -exec {} \;
-./code-generator/generate-custom-resources.sh "${FILTER}"
-./code-generator/generate-mod-rs-files.sh
+./code-generator/create-custom-resources.sh "${FILTER}"
+./code-generator/create-mod-rs-files.sh
 ./code-generator/adjust-cargo-toml.sh
 cargo run --package code-generator --bin lib_rs_generator
