1a0020a5c75da22225cd16153d83e88b85746aba
Sebastian Hoß <seb@xn--ho-hia.de>
2023-11-11 08:27:06 +0100

    add antrea-io/antrea
    Signed-off-by: Sebastian Hoß <seb@xn--ho-hia.de>

---
+++ kube-custom-resources-rs/src/multicluster_crd_antrea_io_v1alpha1/mod.rs
@@ -0,0 +1,8 @@
+pub mod clusterinfoimports;
+pub mod clustersets;
+pub mod gateways;
+pub mod labelidentities;
+pub mod memberclusterannounces;
+pub mod multiclusterconfigs;
+pub mod resourceexports;
+pub mod resourceimports;
