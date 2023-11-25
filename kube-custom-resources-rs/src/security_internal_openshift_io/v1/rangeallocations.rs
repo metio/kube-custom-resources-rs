8d3667dcd434c95da888fb7f5ba762ec87761715
Sebastian Hoß <seb@xn--ho-hia.de>
2023-11-25 12:50:21 +0100

    fix cargo warnings
    Signed-off-by: Sebastian Hoß <seb@xn--ho-hia.de>

--- kube-custom-resources-rs/src/security_internal_openshift_io/v1/rangeallocations.rs
+++ kube-custom-resources-rs/src/security_internal_openshift_io/v1/rangeallocations.rs
@@ -2,6 +2,6 @@
 // kopium command: kopium --docs --filename=./crd-catalog/openshift/api/security.internal.openshift.io/v1/rangeallocations.yaml --derive=Default --derive=PartialEq
 // kopium version: 0.16.2
 
-use kube::CustomResource;
-use serde::{Serialize, Deserialize};
+
+
 
