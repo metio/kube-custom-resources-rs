914a23df8b65506a84b5dbf34f156d5a375dc498
Sebastian Hoß <seb@xn--ho-hia.de>
2024-02-11 12:58:15 +0100

    add kubev2v/forklift
    Signed-off-by: Sebastian Hoß <seb@xn--ho-hia.de>

---
+++ kube-custom-resources-rs/src/forklift_konveyor_io/v1beta1/mod.rs
@@ -0,0 +1,10 @@
+pub mod forkliftcontrollers;
+pub mod hooks;
+pub mod hosts;
+pub mod migrations;
+pub mod networkmaps;
+pub mod openstackvolumepopulators;
+pub mod ovirtvolumepopulators;
+pub mod plans;
+pub mod providers;
+pub mod storagemaps;
