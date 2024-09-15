4e31690965e76ce69a58d3dd67cb02f73797ca0c
Sebastian Hoß <seb@xn--ho-hia.de>
2024-09-15 08:27:23 +0200

    add spidernet-io/egressgateway
    Signed-off-by: Sebastian Hoß <seb@xn--ho-hia.de>

---
+++ kube-custom-resources-rs/src/egressgateway_spidernet_io/v1beta1/mod.rs
@@ -0,0 +1,7 @@
+pub mod egressclusterendpointslices;
+pub mod egressclusterinfos;
+pub mod egressclusterpolicies;
+pub mod egressendpointslices;
+pub mod egressgateways;
+pub mod egresspolicies;
+pub mod egresstunnels;
