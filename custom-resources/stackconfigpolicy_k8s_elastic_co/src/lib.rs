77be037097ecbafb9b7f66163b7498b80a4ef6c4
Sebastian Hoß <seb@xn--ho-hia.de>
2025-09-18 15:43:32 +0200

    re-generate elastic w/ kopium/main
    Signed-off-by: Sebastian Hoß <seb@xn--ho-hia.de>

---
+++ custom-resources/stackconfigpolicy_k8s_elastic_co/src/lib.rs
@@ -0,0 +1,10 @@
+/*!
+This crate contains [kube-rs](https://kube.rs/) compatible bindings for Kubernetes [custom resources](https://kubernetes.io/docs/tasks/extend-kubernetes/custom-resources/custom-resource-definitions/) for the `stackconfigpolicy.k8s.elastic.co` group. Each binding is generated with [kopium](https://github.com/kube-rs/kopium), updated and released weekly.
+
+# Available Custom Resources
+
+## stackconfigpolicy.k8s.elastic.co/v1alpha1
+- `StackConfigPolicy`
+*/
+#[cfg(feature = "v1alpha1")]
+pub mod v1alpha1;
