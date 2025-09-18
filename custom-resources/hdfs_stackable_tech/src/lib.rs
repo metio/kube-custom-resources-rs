086605ee27977608f5f63e1fb44ec7e6b1696804
Sebastian Hoß <seb@xn--ho-hia.de>
2025-09-18 17:50:38 +0200

    re-generate hdfs-operator w/ kopium/main
    Signed-off-by: Sebastian Hoß <seb@xn--ho-hia.de>

---
+++ custom-resources/hdfs_stackable_tech/src/lib.rs
@@ -0,0 +1,10 @@
+/*!
+This crate contains [kube-rs](https://kube.rs/) compatible bindings for Kubernetes [custom resources](https://kubernetes.io/docs/tasks/extend-kubernetes/custom-resources/custom-resource-definitions/) for the `hdfs.stackable.tech` group. Each binding is generated with [kopium](https://github.com/kube-rs/kopium), updated and released weekly.
+
+# Available Custom Resources
+
+## hdfs.stackable.tech/v1alpha1
+- `HdfsCluster`
+*/
+#[cfg(feature = "v1alpha1")]
+pub mod v1alpha1;
